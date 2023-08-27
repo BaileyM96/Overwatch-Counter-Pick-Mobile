f940f0672a4359449703293975016ccf99d94e2c
Bailey Mejia <baileymejia28@gmail.com>
2023-08-27 14:03:15 -0700

    Designed the main page to have the characters display by class

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/Components/Header/Header';
+import Cards from './src/Components/Card/Cards';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      <Cards />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/Components/Classes/Characters.js
@@ -0,0 +1,88 @@
+import Ashe from '../../../assets/images/Damage/Ashe.png';
+import Bastion from '../../../assets/images/Damage/Bastion.png';
+import Cassidy from '../../../assets/images/Damage/Cassidy.png';
+import Echo from '../../../assets/images/Damage/Echo.png';
+import Genji from '../../../assets/images/Damage/Genji.png';
+import Hanzo from '../../../assets/images/Damage/Hanzo.png';
+import Junkrat from '../../../assets/images/Damage/Junkrat.png';
+import Mei from '../../../assets/images/Damage/Mei.png';
+import Pharah from '../../../assets/images/Damage/Pharah.png';
+import Reaper from '../../../assets/images/Damage/Reaper.png';
+import Sojourn from '../../../assets/images/Damage/Sojourn.png';
+import Soldier_76 from '../../../assets/images/Damage/Soldier_76.png';
+import Sombra from '../../../assets/images/Damage/Sombra.png';
+import Symmetra from '../../../assets/images/Damage/Symmetra.png';
+import Torbjorn from '../../../assets/images/Damage/Torbjorn.png';
+import Tracer from '../../../assets/images/Damage/Tracer.png';
+import Widowmaker from '../../../assets/images/Damage/Widowmaker.png';
+
+//Tank Images
+import DVA from '../../../assets/images/Tank/DVA.png';
+import Doomfist from '../../../assets/images/Tank/Doomfist.png';
+import JunkerQueen from '../../../assets/images/Tank/JunkerQueen.png';
+import Orisa from '../../../assets/images/Tank/Orisa.png';
+import Ramattra from '../../../assets/images/Tank/Ramattra.png';
+import Reinhardt from '../../../assets/images/Tank/Reinhardt.png';
+import Roadhog from '../../../assets/images/Tank/Roadhog.png';
+import Sigma from '../../../assets/images/Tank/Sigma.png';
+import Winston from '../../../assets/images/Tank/Winston.png';
+import WreckingBall from '../../../assets/images/Tank/WreckingBall.png';
+import Zarya from '../../../assets/images/Tank/Zarya.png';
+
+//support characters
+import Ana from '../../../assets/images/Support/Ana.png';
+import Baptiste from '../../../assets/images/Support/Baptiste.png';
+import Brigitte from '../../../assets/images/Support/Brigitte.png';
+import Kiriko from '../../../assets/images/Support/Kiriko.png';
+import Lifeweaver from '../../../assets/images/Support/Lifeweaver.png';
+import Lucio from '../../../assets/images/Support/Lucio.png';
+import Mercy from '../../../assets/images/Support/Mercy.png';
+import Moira from '../../../assets/images/Support/Moira.png';
+import Zenyatta from '../../../assets/images/Support/Zenyatta.png';
+
+
+export const damageCharacters = [
+    { name: 'Ashe', image: Ashe },
+    { name: 'Bastion', image: Bastion },
+    { name: 'Cassidy', image: Cassidy },
+    { name: 'Echo', image: Echo },
+    { name: 'Genji', image: Genji },
+    { name: 'Hanzo', image: Hanzo },
+    { name: 'Junkrat', image: Junkrat },
+    { name: 'Mei', image: Mei },
+    { name: 'Pharah', image: Pharah },
+    { name: 'Reaper', image: Reaper },
+    { name: 'Sojourn', image: Sojourn },
+    { name: 'Soldier 76', image: Soldier_76 },
+    { name: 'Sombra', image: Sombra },
+    { name: 'Symmetra', image: Symmetra },
+    { name: 'Torbjorn', image: Torbjorn },
+    { name: 'Tracer', image: Tracer },
+    { name: 'Widowmaker', image: Widowmaker },
+]
+
+export const tankCharacters = [
+    { name: 'DVA', image: DVA },
+    { name: 'Doomfist', image: Doomfist },
+    { name: 'Junker Queen', image: JunkerQueen },
+    { name: 'Orisa', image: Orisa },
+    { name: 'Ramattra', image: Ramattra },
+    { name: 'Reinhardt', image: Reinhardt },
+    { name: 'Roadhog', image: Roadhog },
+    { name: 'Sigma', image: Sigma },
+    { name: 'Winston', image: Winston },
+    { name: 'Wrecking Ball', image: WreckingBall },
+    { name: 'Zarya', image: Zarya },
+]
+
+export const supportCharacters = [
+    { name: 'Ana', image: Ana },
+    { name: 'Baptiste', image: Baptiste },
+    { name: 'Brigitte', image: Brigitte },
+    { name: 'Kiriko', image: Kiriko },
+    { name: 'Lifeweaver', image: Lifeweaver },
+    { name: 'Lucio', image: Lucio },
+    { name: 'Mercy', image: Mercy },
+    { name: 'Moira', image: Moira },
+    { name: 'Zenyatta', image: Zenyatta },
+]
